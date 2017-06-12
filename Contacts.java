e59f4fa075801efc3c7245fc3485a5ab04d00f9e
PiguetNico <piguetnico@gmail.com>
2017-06-12 10:02:46 +0200

    Change structure, add methods for jlist and add handling for errors null

--- Contacts.java
+++ Contacts.java
@@ -1,6 +1,5 @@
 package Contacts;
 
-import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -10,10 +9,10 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -32,19 +31,113 @@ public class Contacts extends JPanel {
 	private JButton modify = new JButton("Modify");
 	private JButton delete = new JButton("Delete");
 	private JPanel panel = new JPanel();
-
-
-	private Vector<String> list = new Vector<String>();
-	//	private String line;
-	//	File contacts = new File("Contacts.txt");
-	//	FileReader fr = new FileReader(contacts);
-	//	BufferedReader br = new BufferedReader(fr);
+	protected JList jlist;
+	private JScrollPane scroll;
+	protected String lineSelected;
+	protected String lineSelectedToModify;
+	protected Vector<String> list = new Vector<String>();
 
 
 	public Contacts(JFrame frame) throws IOException{
 
 		setBackground(Color.BLACK);
 
+		initialize();
+
+		getNewListContact();
+
+
+		// ADD ACTION FOR BUTTONS WITH ANONYMOUS CLASS
+		back.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((CardLayout) frame.getContentPane().getLayout()).show(frame.getContentPane(), "home");
+			}
+		});
+
+
+		add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((CardLayout) frame.getContentPane().getLayout()).show(frame.getContentPane(), "addContact");
+			}
+		});
+
+
+		modify.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+				lineSelectedToModify = jlist.getSelectedValue().toString();
+				((CardLayout) frame.getContentPane().getLayout()).show(frame.getContentPane(), "modifyContact");
+				}catch (Exception e1){
+					JOptionPane.showMessageDialog(null, "FAILED TO MODIFY CONTACT !!! ");
+				}
+			}
+		});
+
+
+		delete.addActionListener (new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				// DELETE CONTACT IN THE TXT FILE
+				try {
+					File inputFile = new File("Contacts.txt");
+					File tempFile = new File("ContactsTemp.txt");
+
+					BufferedReader br = new BufferedReader(new FileReader(inputFile));
+					BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+
+					lineSelected = jlist.getSelectedValue().toString();
+					String currentLine;
+
+					int i = 0;
+					while((currentLine = br.readLine()) != null) {
+						String trimmedLine = currentLine.trim();
+						if(!trimmedLine.contains(lineSelected)) { //startswith()
+							if(i == 0){
+								bw.write(currentLine); 
+							}
+							else{
+								bw.write("\n" + currentLine); 
+							}
+							i++;
+						}
+					}  
+
+					// DELETE CONTACT IN THE JLIST
+					int index = jlist.getSelectedIndex();
+					if (index != -1){
+						list.remove(index);
+					}
+
+					bw.close();
+
+					// METHODS TO REFRESH THE JLIST
+					Collections.sort(list);
+					jlist.setListData(list);
+					jlist.setSelectedIndex(0);
+
+
+					if(!inputFile.delete()) {
+						JOptionPane.showMessageDialog(null, "IMPOSSIBLE TO RENAME TEMPFILE");
+						return;
+					}
+					if(!tempFile.renameTo(inputFile))
+						JOptionPane.showMessageDialog(null, "IMPOSSIBLE TO RENAME TEMPFILE");
+
+
+					br.close();
+					tempFile.renameTo(inputFile);	
+				}
+
+				catch(Exception e1) {
+					JOptionPane.showMessageDialog(null, "FAILED TO DELETE CONTACT !!! ");
+					e1.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	private void initialize() {
 		// BUTTONS
 		back.setPreferredSize(new Dimension(90,90));
 		back.setBackground(Color.BLACK);
@@ -75,143 +168,34 @@ public class Contacts extends JPanel {
 		panel.add(modify);
 		panel.add(delete);
 		add(panel);
-
-
-		// LIST CONTACTS
-		//		while ((line = br.readLine()) != null) {
-		//			list.add(line);
-		//		}
-
-		getNewListContact();
-		JList jlist = new JList(list);
-		jlist.setFont(new Font("Arial",Font.BOLD,25));
-		jlist.setForeground(Color.WHITE);
-		jlist.setBackground(Color.BLACK);
-		jlist.setFixedCellHeight(44);
-		JScrollPane scroll = new JScrollPane (jlist);
-		scroll.setPreferredSize(new Dimension(450, 650));
-		add(scroll);
-
-
-
-
-		// ADD ACTION FOR BUTTONS WITH ANONYMOUS CLASS
-		back.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				((CardLayout) frame.getContentPane().getLayout()).show(frame.getContentPane(), "home");
-			}
-		});
-
-
-		add.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				((CardLayout) frame.getContentPane().getLayout()).show(frame.getContentPane(), "addContact");
-			}
-		});
-
-
-		modify.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				((CardLayout) frame.getContentPane().getLayout()).show(frame.getContentPane(), "modifyContact");
-			}
-		});
-
-
-		delete.addActionListener (new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				// DELETE CONTACT IN THE JLIST
-				int index = jlist.getSelectedIndex();
-				if (index != 0){
-					list.remove(index);
-				}
-
-				// DELETE CONTACT IN THE TXT FILE
-				try {
-					File inputFile = new File("Contacts.txt");
-					File tempFile = new File("ContactsTemp.txt");
-
-					BufferedReader br = new BufferedReader(new FileReader(inputFile));
-					BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
-
-					String lineToRemove = jlist.getSelectedValue().toString();
-					System.out.println(jlist.getSelectedValue());
-					System.out.println(lineToRemove);
-					String currentLine;
-
-					int i = 0;
-					while((currentLine = br.readLine()) != null) {
-						String trimmedLine = currentLine.trim();
-						if(!trimmedLine.contains(lineToRemove)) { //startswith()
-							if(i == 0){
-								bw.write(currentLine); 
-							}
-							else{
-								bw.write("\n" + currentLine); 
-							}
-							i++;
-						}
-					}  
-					
-				
-					bw.close();
-
-
-					if(!inputFile.delete())
-					{
-						JOptionPane.showMessageDialog(null, "IMPOSSIBLE TO RENAME TEMPFILE");
-						return;
-					}
-					if(!tempFile.renameTo(inputFile))
-						JOptionPane.showMessageDialog(null, "IMPOSSIBLE TO RENAME TEMPFILE");
-					tempFile.renameTo(inputFile);
-
-					br.close();
-				}
-				catch(Exception e1)
-				{
-					JOptionPane.showMessageDialog(null, "FAILED TO DELETE CONTACT !!! ");
-					e1.printStackTrace();
-				}
-			}
-		});
 	}
 
 
-	public void getNewListContact() throws IOException{
+	private void getNewListContact() throws IOException{
 		String line="";
 		File contacts = new File("Contacts.txt");
 		FileReader fr = new FileReader(contacts);
 		BufferedReader br = new BufferedReader(fr);
 
 		while ((line = br.readLine()) != null) {
-//			String[] listWithSpace = line.split(":");
-//			list.add(listWithSpace[0] + "  " + listWithSpace[1] + "    " + listWithSpace[2]);
 			list.add(line);
 		}
+		fr.close();
 
-		fr.close();	
+		// SORT THE LIST A-Z
+		Collections.sort(list);
+		jlist = new JList(list);
+
+		// ALWAYS SELECT THE FIRST ROW IN THE JLIST
+		jlist.setSelectedIndex(0);
+		jlist.setFont(new Font("Arial",Font.BOLD,25));
+		jlist.setForeground(Color.WHITE);
+		jlist.setBackground(Color.BLACK);
+
+		// MAKE SPACE BETWEEN LINES
+		jlist.setFixedCellHeight(44);
+		scroll = new JScrollPane (jlist);
+		scroll.setPreferredSize(new Dimension(450, 650));
+		add(scroll);
 	}
-
-
-
-
-
-
-	//	class MaListe {
-	//		private String name;
-	//		private String lastName;
-	//		private String number;
-	//
-	//
-	//		public MaListe(String n, String ln, String numb) {
-	//			this.name = n;
-	//			this.lastName = ln;
-	//			this.number=numb;
-	//
-	//		}
-	//		public String toString() {
-	//			return this.lastName.toUpperCase() + "  " + this.name + " : " + this.number;
-	//		}
-	//	}
 }
